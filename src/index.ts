dd85fd3aa30151da350778015c4c3e386c3b01c2
Paolo <str237@gmail.com>
2021-09-21 23:38:11 +0200

    Handle resize

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,44 @@
 import './main.css'
 import * as THREE from 'three'
 
+interface SimpleRect {
+  height: number
+  width: number
+}
+
+const fits = (ref: SimpleRect, other: SimpleRect): boolean =>
+  other.width <= ref.width && other.height <= ref.height
+
+const getViewSimpleRect = ():SimpleRect => ({
+  height: window.innerHeight,
+  width: window.innerWidth,
+})
+
 const aspect = 16 / 9
 const canvas = document.getElementById('canvas')
 
 const renderer = new THREE.WebGLRenderer({
   canvas
 })
-renderer.setSize( window.innerWidth, window.innerHeight );
+
+const onResize = () => {
+  const v = getViewSimpleRect()
+  const opt1: SimpleRect = {
+    height: v.height,
+    width: v.height * aspect
+  }
+  const opt2: SimpleRect = {
+    height: v.width / aspect,
+    width: v.width
+  }
+  const desired = fits(v, opt1) ? opt1 : opt2
+
+  renderer.setSize(desired.width, desired.height);
+}
+
+onResize()
+
+const resizeListener = window.addEventListener('resize', onResize)
 
 const camera = new THREE.PerspectiveCamera( 45, aspect, 1, 500 );
 camera.position.set( 0, 0, 100 );
